2ed84406700ec7c5cc1f846f13fe8a5877368342
DamV2 <DamV2@MAQUETA>
2023-11-14 19:25:37 +0200

    TopDown first

---
+++ Assets/Scripts/player/PlayerMovement.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class playerMovement : MonoBehaviour
+{
+    [SerializeField]
+    private float _speed;
+
+    [SerializeField]
+    private float _rotationSpeed;
+
+    private Rigidbody2D _rigidbody;
+    private Vector2 _movementInput;
+    private Vector2 _smoothedMovementInput;
+    private Vector2 _movementInputSmoothVelocity;
+
+    private void Awake()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    private void FixedUpdate()
+    {
+        SetPlayerVelocity();
+        RotateInDirectionOfInput();
+    }
+    //private void Update()
+    //{
+    //       Vector2 direction = new Vector2(Input)
+    //}
+
+    private void SetPlayerVelocity()
+    {
+        _smoothedMovementInput = Vector2.SmoothDamp(
+                    _smoothedMovementInput,
+                    _movementInput,
+                    ref _movementInputSmoothVelocity,
+                    0.1f);
+
+        _rigidbody.velocity = _smoothedMovementInput * _speed;
+    }
+
+    private void RotateInDirectionOfInput()
+    {
+        if (_movementInput != Vector2.zero)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(transform.forward, _smoothedMovementInput);
+            Quaternion rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime);
+
+            _rigidbody.MoveRotation(rotation);
+        }
+    }
+
+}
+
