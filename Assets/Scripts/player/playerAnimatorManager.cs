c973c28dbaf154b4daef985465c66b00cde48111
Fabi <fgalindo.oliveira@gmail.com>
2023-11-23 23:10:41 +0100

    files

---
+++ Assets/Scripts/player/playerAnimatorManager.cs
@@ -0,0 +1,20 @@
+using UnityEngine;
+
+namespace Scrips.player
+{
+    public class PlayerAnimatorManager : MonoBehaviour
+    {
+        public
+        // Start is called before the first frame update
+        void Start()
+        {
+        
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+        
+        }
+    }
+}
