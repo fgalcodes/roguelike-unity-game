ca05d020d75823adb987c018c5d9c30be1ee4fb5
Adrià Boguñá <125764274+AdriaBoguna@users.noreply.github.com>
2023-12-18 23:56:51 +0100

    Count keys

--- Assets/Scripts/Doors/KeyInventory.cs
+++ Assets/Scripts/Doors/KeyInventory.cs
@@ -1,15 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
 
 public class KeyInventory : MonoBehaviour
 {
     public PlayerKeys playerKeys;
+    public TMP_Text numberKeyText;
 
 
     private void Awake()
     {
         playerKeys.keys = 0;
+        UpdateKeyText();
     }
 
     private void OnTriggerEnter2D(Collider2D collision)
@@ -19,6 +23,7 @@ public class KeyInventory : MonoBehaviour
             Debug.Log("Key +1");
 
             playerKeys.keys++;
+            UpdateKeyText();
 
             collision.gameObject.SetActive(false);
 
@@ -32,9 +37,23 @@ public class KeyInventory : MonoBehaviour
             Debug.Log("Key -1");
 
             playerKeys.keys--;
+            UpdateKeyText();
 
             collision.gameObject.SetActive(false);
 
+        }
+    }
+
+    
+    private void UpdateKeyText()
+    {
+        if (numberKeyText != null)
+        {
+            numberKeyText.text = playerKeys.keys.ToString();
+        }
+        else
+        {
+            Debug.Log("No se ha asignado el texto para mostrar las llaves en el canvas.");
         }
     }
 }
