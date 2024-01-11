9203248d4c8c66d66ffcdee6d838825f9c7fc792
fgalcodes <fabian.galindo.7e6@itb.cat>
2024-01-11 17:58:16 +0100

    tower health damage

--- Assets/Scripts/TowerHealth.cs
+++ Assets/Scripts/TowerHealth.cs
@@ -7,30 +7,46 @@ public class TowerHealth : MonoBehaviour
     //[SerializeField] private Healthbar healthBar;
     [SerializeField] private float maxHealth = 100f;
     private float currentHealth;
-    public enum States
-    {
-        Run,
-        Attack,
-        Die
-    }
-    public States currentState;
 
+    private void Start()
+    {
+        currentHealth = maxHealth;
+    }
     public void TomarDaño(float daño)
     {
         currentHealth -= daño;
 
         if (currentHealth <= 0)
         {
-            currentState = States.Die;
+            GetComponent<SpriteRenderer>().color = Color.red;
             DestroyEnemy();
+            //Debug.Log(currentHealth);
+            //Debug.Log(daño);
         }
 
         else
         {
             //healthBar.UpdateHealthbar(maxHealth, currentHealth, currentHealth + daño);
-            Debug.Log("damage");
+            Debug.Log("damage" + currentHealth);
+            GetComponent<SpriteRenderer>().color = Color.red;
+
+
+            // Inicia una corutina para ocultar la imagen después de cierto tiempo
+            StartCoroutine(HitDamageEffect(.6f));
         }
     }
+
+    IEnumerator HitDamageEffect(float duration)
+    {
+        Debug.Log("hit");
+
+        // Espera el tiempo especificado antes de ocultar la imagen de sangre
+        yield return new WaitForSeconds(duration);
+
+        // Oculta la imagen de sangre después del tiempo especificado
+        GetComponent<SpriteRenderer>().color = Color.white;
+    }
+
     void DestroyEnemy()
     {
         Destroy(gameObject);
