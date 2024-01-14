43bb271bc4f4e0de38d21ca4cb8bfe695bc4e100
Adrià Boguñá <125764274+AdriaBoguna@users.noreply.github.com>
2024-01-14 14:46:14 +0100

    Choise

--- Assets/DialogController.cs
+++ Assets/DialogController.cs
@@ -2,18 +2,20 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class DialogController : MonoBehaviour
 {
-    public Text dialogText;
+    public TMP_Text dialogText;
     public Button nextButton;
+    public Button prevButton;
     public Button keyButton;
     public Button coinsButton;
 
     private string[] paragraphs = {
         "¡Ah, viajero intrépido! Te encuentras ante la encrucijada de tu destino. En mi humilde puesto de comercio, ofrezco dos tesoros que pueden cambiar el rumbo de tu travesía. La elección, sin duda, marcará tu camino en esta oscura mazmorra.",
-        "Opción 1 - Llave Misteriosa:\n¿Una llave misteriosa, forjada en las entrañas de la antigua magia? Nadie sabe a qué puertas abre, pero dicen que detrás se ocultan secretos olvidados y riquezas inimaginables. ¿Te atreves a desvelar los misterios que aguardan tras ella?",
-        "Opción 2 - 100 Monedas:\n¿O quizás prefieras el tintineo de las monedas, brillando con la promesa de poder y gloria? Con cien monedas, podrías comprar el favor de los seres sombríos que moran en estas profundidades, o invertirlas sabiamente en equipo que te haga más resistente a las artimañas de la mazmorra.",
+        "Opción 1 - Llave Misteriosa: ¿Una llave misteriosa, forjada en las entrañas de la antigua magia? Nadie sabe a qué puertas abre, pero dicen que detrás se ocultan secretos olvidados y riquezas inimaginables. ¿Te atreves a desvelar los misterios que aguardan tras ella?",
+        "Opción 2 - 100 Monedas: ¿O quizás prefieras el tintineo de las monedas, brillando con la promesa de poder y gloria? Con cien monedas, podrías comprar el favor de los seres sombríos que moran en estas profundidades, o invertirlas sabiamente en equipo que te haga más resistente a las artimañas de la mazmorra.",
         "Recuerda, valiente aventurero, cada elección que hagas resonará en las sombras de este laberinto. El destino te aguarda, pero solo elige sabiamente aquellos tesoros que iluminarán tu camino hacia la victoria. ¡Buena suerte!"
     };
 
@@ -25,47 +27,97 @@ public class DialogController : MonoBehaviour
         nextButton.onClick.AddListener(ShowNextParagraph);
         keyButton.onClick.AddListener(ShowKey);
         coinsButton.onClick.AddListener(ShowCoins);
-        HideButtons();
+        prevButton.onClick.AddListener(ShowPrevParagraph);
+        UpdateButtons();
     }
 
-    void ShowNextParagraph()
+    public void ShowNextParagraph()
     {
-        currentParagraph++;
 
-        if (currentParagraph < paragraphs.Length)
+        if (currentParagraph < paragraphs.Length - 1)
         {
+            currentParagraph++;
             dialogText.text = paragraphs[currentParagraph];
+            UpdateButtons();
         }
         else
         {
             HideButtons();
-            dialogText.text = "¡Has llegado al final!";
-            keyButton.gameObject.SetActive(true);
-            coinsButton.gameObject.SetActive(true);
+            dialogText.text = "";
         }
+        //prevButton.gameObject.SetActive(currentParagraph > 0);
+    }
+
+    public void ShowPrevParagraph()
+    {
+        if (currentParagraph > 0)
+        {
+            currentParagraph--;
+            dialogText.text = paragraphs[currentParagraph];
+            UpdateButtons();
+        }
+
+        //prevButton.gameObject.SetActive(currentParagraph > 0);
     }
 
     void ShowKey()
     {
-        // Acciones para la opción "Key"
         HideCanvas();
     }
 
     void ShowCoins()
     {
-        // Acciones para la opción "Coins"
         HideCanvas();
+    }
+
+    void UpdateButtons()
+    {
+        if (currentParagraph == 0)
+        {
+            nextButton.gameObject.SetActive(true);
+            prevButton.gameObject.SetActive(false);
+            keyButton.gameObject.SetActive(false);
+            coinsButton.gameObject.SetActive(false);
+        }
+        else if (currentParagraph == paragraphs.Length - 1)
+        {
+            nextButton.gameObject.SetActive(false);
+            prevButton.gameObject.SetActive(true);
+            keyButton.gameObject.SetActive(true);
+            coinsButton.gameObject.SetActive(true);
+        }
+        else
+        {
+            nextButton.gameObject.SetActive(true);
+            prevButton.gameObject.SetActive(true);
+            keyButton.gameObject.SetActive(false);
+            coinsButton.gameObject.SetActive(false);
+        }
     }
 
     void HideButtons()
     {
         nextButton.gameObject.SetActive(false);
+        keyButton.gameObject.SetActive(true);
+        coinsButton.gameObject.SetActive(true);
+
+        Time.timeScale = 0f;
+    }
+
+    void ShowButtons()
+    {
+        nextButton.gameObject.SetActive(true);
+        prevButton.gameObject.SetActive(false);
         keyButton.gameObject.SetActive(false);
         coinsButton.gameObject.SetActive(false);
+
+        Time.timeScale = 1f;
     }
 
     void HideCanvas()
     {
+        Time.timeScale = 1f;
+
         gameObject.SetActive(false);
     }
 }
