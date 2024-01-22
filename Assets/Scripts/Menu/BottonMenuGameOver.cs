95a72085808ca39b7b53f0d1536f32712e682118
Adrià Boguñá <125764274+AdriaBoguna@users.noreply.github.com>
2024-01-22 23:04:22 +0100

    Pause Menu

--- Assets/Scripts/Menu/BottonMenuGameOver.cs
+++ Assets/Scripts/Menu/BottonMenuGameOver.cs
@@ -5,21 +5,18 @@ using UnityEngine.SceneManagement;
 
 public class BottonMenuGameOver : MonoBehaviour
 {
-    // Método para reiniciar el nivel
     public void RestartLevel()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
     }
 
-    // Método para volver al menú principal
     public void BackToMenu()
     {
         Time.timeScale = 1.0f;
         SceneManager.LoadScene("Menu");
     }
 
-    // Método para salir del juego
     public void ExitGame()
     {
         UnityEngine.Application.Quit();
